f9f035c65bb7ca2ff3b5e3ffe2c28b6c15f5eb90
ezzat <ezatce117@outlook.com>
2022-09-13 12:22:55 +0300

    week6

--- week7/16-8/algorithm2/reverse.js
+++
@@ -1,6 +0,0 @@
-const reverse = function (str) {
-  return [...str].map((char, index) => str[str.length - 1 - index]).join("");
-};
-
-console.log(reverse("dog")); //should return "god"
-console.log(reverse("race car")); //should return "rac ecar"
